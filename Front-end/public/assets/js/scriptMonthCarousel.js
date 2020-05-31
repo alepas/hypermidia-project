567775bdaabc416582d0573b14e4c71c129f63d4
stepolimi <37816183+stepolimi@users.noreply.github.com>
2020-05-31 19:04:43 +0200

    faq, person and all events

--- Front-end/public/assets/js/scriptMonthCarousel.js
+++ Front-end/public/assets/js/scriptMonthCarousel.js
@@ -13,8 +13,7 @@ function currentSlide(n) {
 
 function showSlides(n) {
     var i;
-    var slides = document.getElementsByClassName("carousel-itema");
-    var dots = document.getElementsByClassName("carousel-dot");
+    var slides = document.getElementsByClassName("month-itema");
     if (n > slides.length) {
         slideIndex = 1
     }
@@ -24,9 +23,6 @@ function showSlides(n) {
     for (i = 0; i < slides.length; i++) {
             slides[i].style.display = "none";
         }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
+
     slides[slideIndex-1].style.display = "block";
-    dots[slideIndex-1].className += " active";
 }
