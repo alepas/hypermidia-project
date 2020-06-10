1da1889217a31e1ef18d7856ff9260ef7c110e13
alepas <alessandra.pasini@mail.polimi.it>
2020-06-11 01:33:46 +0200

    Update DataLayer.js

--- service/DataLayer.js
+++ service/DataLayer.js
@@ -5,13 +5,7 @@ const sqlDbFactory = require("knex");
 let sqlDb = sqlDbFactory({
 debug: true,
 client: "pg",
-connection: {
-    host: process.env.DATABASE_URL,
-    database: 'd30rkc6815nm22',
-    port: 5432,
-    user: 'tevjuzdetgjkdz',
-    password: '3bf167276e24a6a03c82d2918752ffe9087abaeb6fe1a8e4c08d57943b3db7da'
-},
+connection: process.env.DATABASE_URL,
 ssl: true
 });
 
