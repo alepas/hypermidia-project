4a9e9d5c9994a2b776079c805e79ff8418757eaa
stepolimi <37816183+stepolimi@users.noreply.github.com>
2020-06-23 19:20:49 +0200

    update month and people

--- public/assets/js/scriptMonthCarousel.js
+++ public/assets/js/scriptMonthCarousel.js
@@ -25,4 +25,26 @@ function showSlides(n) {
         }
 
     slides[slideIndex-1].style.display = "block";
+
+    changeMonth(slideIndex);
+}
+
+function changeMonth(month){
+  fetch("../../v1/events?limit=12&offset=0&month=" + month)
+  .then(function(response) {
+      if (!response.ok) 
+      throw new Error("HTTP error, status = " + response.status);
+      return response.json();
+  })
+  .then(function(json) {
+      for (var i = 0; i < json.length; i++) {
+          var event_desc = document.getElementById("event_desc_" + i);
+          let {title, image, id_event, date} = json[i];
+
+          document.getElementById("event_" + i).style.display = "block";
+          document.getElementById("event_img_" + i).src = `${image}`;
+      //  document.getElementById("event_img_" + i).style.height = "300";
+          event_desc.innerHTML =  `${title}`;
+      }
+  });
 }
