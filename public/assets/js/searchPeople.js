40891387e0d7912a36c92b5923b8ce058271a5df
stepolimi <37816183+stepolimi@users.noreply.github.com>
2020-06-25 19:21:47 +0200

    people filter

---
+++ public/assets/js/searchPeople.js
@@ -0,0 +1,19 @@
+var people = [];
+
+function savePeople(person){
+    people.push(person);
+}
+
+function filterPeople(){
+    for(var i=0; i<8; i++){
+        document.getElementById("person_" + i).style.display = "none";
+    }
+
+    var filter = document.getElementById("filter").value;
+
+    for(var i=0; i< people; i++){
+        if(people[i].includes(filter))
+            document.getElementById("person_" + i).style.display = "block";
+    }
+
+}
