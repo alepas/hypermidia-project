a391e0b62c2649624489d6fe12f75081e3b4d792
stepolimi <37816183+stepolimi@users.noreply.github.com>
2020-06-26 18:45:54 +0200

    Update scriptPagination.js

--- public/assets/js/scriptPagination.js
+++ public/assets/js/scriptPagination.js
@@ -68,19 +68,39 @@ indexs.forEach((index, i) => {
       var offset = (index.innerHTML - 1) * 9;
       console.log(offset);
       console.log(index.innerHTML);
-      fetch("../../v1/events?limit=9&offset=" + offset)
+
+      var radios = document.getElementsByClassName("form-check-input");
+      var topic;
+      var period;
+  
+      for(var i=0; i< radios.length; i++){
+          if(radios[i].checked){
+              if(radios[i].name == "topicRadio")
+                  topic = radios[i].value;
+              else
+                  period = radios[i].value;
+          }
+      }
+  
+      var fet = "../../v1/events?limit=9&offset=" + offset;
+      if(topic != null)
+          fet = fet + "&" + "topic=" + topic;
+      if(period != null)
+          fet = fet + "&" + "period=" + period;
+  
+      fetch(fet)
       .then(function(response) {
       if (!response.ok) 
           throw new Error("HTTP error, status = " + response.status);
       return response.json();
       })
       .then(function(json) {
-        for (var i = 0; i < 9; i++) {
-          document.getElementById("event_" + i).style.display = "none";
-        }
+          for(var i=0; i<9; i++)
+              document.getElementById("event_" + i).style.display = "none";
+  
           for (var i = 0; i < json.length; i++) {
               let {title, image, id_event} = json[i];
-
+  
               document.getElementById("event_" + i).style.display = "block";
               document.getElementById("event_desc_" + i).innerHTML =  `${title}`;
               document.getElementById("event_img_" + i).src = `${image}`;
