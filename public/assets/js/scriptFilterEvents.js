016912a3f1afdb0ab2fb3cc1640d9eb254d82725
stepolimi <37816183+stepolimi@users.noreply.github.com>
2020-06-26 14:09:46 +0200

    event filters

--- public/assets/js/scriptFilterEvents.js
+++ public/assets/js/scriptFilterEvents.js
@@ -41,3 +41,29 @@ function filterEvents(){
         }
     });
 }
+
+function clearFilters(){
+    var radios = document.getElementsByClassName("form-check-input");
+
+    for(var i=0; i< radios.length; i++){
+        radios[i].checked = false;
+    }
+
+    fetch("../v1/events?limit=9&offset=0")
+    .then(function(response) {
+    if (!response.ok) 
+        throw new Error("HTTP error, status = " + response.status);
+    return response.json();
+    })
+    .then(function(json) {
+        for (var i = 0; i < json.length; i++) {
+            let {title, image, id_event} = json[i];
+
+            document.getElementById("event_" + i).style.display = "block";
+            document.getElementById("event_desc_" + i).innerHTML =  `${title}`;
+            document.getElementById("event_img_" + i).src = `${image}`;
+            document.getElementById("event_link_" + i).onclick = function() 
+                {localStorage["id_event"] = `${id_event}`;};
+        }
+    });
+}
