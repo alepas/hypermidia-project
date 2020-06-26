0b84e9e55607f42637d45dca4a255dfc7906f88c
stepolimi <37816183+stepolimi@users.noreply.github.com>
2020-06-26 16:55:22 +0200

    offset change

--- public/assets/js/scriptPagination.js
+++ public/assets/js/scriptPagination.js
@@ -12,5 +12,30 @@ indexs.forEach((index, i) => {
       c.classList.add('flip')
     }
     cur = i
+
+    var offset = index.value * 12;
+    console.log(offset);
+
+    fetch("../../v1/services?limit=12&offset=" + offset)
+    .then(function(response) {
+    if (!response.ok) 
+        throw new Error("HTTP error, status = " + response.status);
+    return response.json();
+    })
+
+    .then(function(json) {
+      for (var i = 0; i < 12; i++) {
+        document.getElementById("service_" + i).style.display = "none";
+      }
+      for (var i = 0; i < json.length; i++) {
+          let {title, photo, id_service} = json[i];
+
+          document.getElementById("service_desc_" + i).innerHTML =  `${title}`;
+          document.getElementById("service_" + i).style.display = "block";
+          document.getElementById("service_img_" + i).src = `${photo}`;
+          document.getElementById("service_link_" + i).onclick = function() 
+              {localStorage["id_service"] = `${id_service}`;};
+      }
+  });
   })
 })
