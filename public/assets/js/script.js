9b7b0d26b309ada247e25a20d0c72562ad53708f
alepas <alessandra.pasini@mail.polimi.it>
2020-06-27 15:05:06 +0200

    updates

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -41,16 +41,6 @@ function showSlides(n) {
     }
     slides[slideIndex-1].style.display = "block";
     dots[slideIndex-1].className += " active";
-
-    
-    $(document).ready(function(){
-        $('.carousel-containera').slick({
-            autoplay: true,
-            autoplaySpeed: 3000,
-            dots: true,
-            arrows: false
-        });
-    });
 }
 
 function hideSlides(){
